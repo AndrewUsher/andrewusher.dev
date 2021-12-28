11da4ec079a9f281b48b9338711d9aaa63084eca
Andrew Usher <andrewusher00@gmail.com>
2021-12-28 14:43:56 -0600

    init

---
+++ app/components/root/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export function Header() {
+  return (
+    <header className="max-w-screen-xl mx-auto py-4">
+      <h1 className="text-4xl font-bold">Andrew Usher</h1>      
+    </header>
+  )
+}
---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import type { LinksFunction, MetaFunction } from "remix";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import { Header } from "./components/root/Header";
+import styles from './tailwind.css';
+
+export const meta: MetaFunction = () => {
+  return { title: "Andrew Usher" };
+};
+
+export const links: LinksFunction = () => {
+  return [{rel: "stylesheet", href: styles}]
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Header />
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
