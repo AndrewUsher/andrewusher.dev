e300f9ac9a9ec377fce5e85444296a407838f024
Andrew Usher <andrewusher00@gmail.com>
2021-12-29 11:53:39 -0600

    feat: add initial homepage section

--- app/components/root/Header.tsx
+++ app/components/root/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Header () {
   return (
-    <header className="max-w-screen-xl mx-auto py-4">
+    <header className="max-w-screen-xl mx-auto py-4 px-4">
       <h1 className="text-4xl font-bold">Andrew Usher</h1>
     </header>
   )
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import * as React from 'react'
+import { Link } from 'remix'
+
+function Paragraph ({ children }: {children: React.ReactNode}) {
+  return (
+    <p className="text-2xl max-w-5xl mb-4">{children}</p>
+  )
+}
+
+function ExternalLink ({ href, children }: {children: string, href: string}) {
+  return (
+    <a className="underline" href={href}>{children}</a>
+  )
+}
+
+function IntroLinkButton ({ children, to }: {children: string, to: string}) {
+  return (
+    <Link className="bg-sky-400 hover:bg-sky-600 hover:text-white mr-8 px-4 py-4 text-lg ease-in-out" to={to}>{children}</Link>
+  )
+}
 
 export default function Index () {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-
-    </div>
+    <>
+      <div className="max-w-screen-xl mx-auto py-4 px-4">
+        <Paragraph>
+          Howdy! During the day, I am a systems engineer at <ExternalLink href="https://www.autozone.com/">AutoZone</ExternalLink>, primarily leading a small team of React developers on the B2C web application.
+        </Paragraph>
+        <Paragraph>
+          During my free time, I like dabbling around with new front end technologies by creating my own projects. When I&apos;m not coding, I like to play basketball and ride my bike throughout downtown Memphis.
+        </Paragraph>
+        <div className="my-12">
+          <IntroLinkButton to="/blog">Go To Blog</IntroLinkButton>
+          <IntroLinkButton to="/contact">Get In Touch</IntroLinkButton>
+        </div>
+      </div>
+    </>
   )
 }
