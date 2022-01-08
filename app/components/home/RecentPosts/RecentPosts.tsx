fe1f2bcb0e0ff33ec969f89c12ec0aeb00444a3d
Andrew Usher <andrewusher00@gmail.com>
2022-01-08 13:17:17 -0600

    feat: add text underline to posts

--- app/components/home/RecentPosts/RecentPosts.tsx
+++ app/components/home/RecentPosts/RecentPosts.tsx
@@ -21,7 +21,7 @@ export function RecentPosts ({ posts }: Props) {
       <h2 className="text-4xl font-bold mb-8 dark:text-white">Recent Blog Posts</h2>
       {posts.map(post => (
         <article key={post.title} className="mb-6">
-          <h3 className="text-2xl font-semibold dark:text-white">
+          <h3 className="text-2xl mb-1 font-semibold dark:text-white underline decoration-sky-400 decoration-[0.25rem] motion-safe:transition-all motion-safe:duration-200 hover:decoration-[0.5rem] focus:decoration-[0.5rem hover:decoration-sky-400/50 focus:decoration-sky-600/50">
             <Link to={`/blog/${post.slug}`}>
               {post.title}
             </Link>
