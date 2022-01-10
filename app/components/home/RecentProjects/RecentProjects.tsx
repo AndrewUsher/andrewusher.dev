584c70f7d1670aee4c5a71b298f1e74b50fa2e4e
Andrew Usher <andrewusher00@gmail.com>
2022-01-09 20:04:29 -0600

    fix: update styling for recent projects

--- app/components/home/RecentProjects/RecentProjects.tsx
+++ app/components/home/RecentProjects/RecentProjects.tsx
@@ -15,10 +15,10 @@ export function RecentProjects ({ projects }: Props) {
       <h2 className="text-4xl font-bold mb-8 dark:text-white">Recent Projects</h2>
       {projects.map(project => (
         <article key={project.title} className="mb-6">
-          <h3 className="text-2xl mb-1 font-semibold dark:text-white underline decoration-sky-400 decoration-[0.25rem] motion-safe:transition-all motion-safe:duration-200 hover:decoration-[0.5rem] focus:decoration-[0.5rem hover:decoration-sky-400/50 focus:decoration-sky-600/50">
+          <h3 className="text-2xl mb-4 font-semibold dark:text-white underline decoration-sky-400 decoration-[0.25rem] motion-safe:transition-all motion-safe:duration-200 hover:decoration-[0.5rem] focus:decoration-[0.5rem hover:decoration-sky-400/50 focus:decoration-sky-600/50">
             {project.title}
           </h3>
-          <p>{project.summary}</p>
+          <p className="text-xl leading-8 dark:text-slate-300">{project.summary}</p>
         </article>
       ))}
     </section>
