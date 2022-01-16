56bdc9aa4682f854ba37b0c593366411508c3a31
Andrew Usher <andrewusher00@gmail.com>
2022-01-16 14:59:25 -0600

    feat: switch favicons and add app manifest

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,11 @@ export default function App () {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#7dd3fc" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0284c7" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+
         <Meta />
         <Links />
       </head>
