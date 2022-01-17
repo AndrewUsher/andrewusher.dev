6b3628052c212b13a48858b5c3ad9348bba31a6e
Andrew Usher <andrewusher00@gmail.com>
2022-01-16 22:34:12 -0600

    feat: add more info to uses page

--- app/routes/uses.tsx
+++ app/routes/uses.tsx
@@ -4,7 +4,7 @@ import UsesPageContent from '../content/uses.mdx'
 export default function AboutPage () {
   return (
     <>
-      <div className="prose lg:prose-xl max-w-screen-xl mx-auto py-4 px-4">
+      <div className="prose lg:prose-xl max-w-screen-xl mx-auto py-12 px-4 dark:prose-invert">
         <UsesPageContent />
       </div>
     </>
