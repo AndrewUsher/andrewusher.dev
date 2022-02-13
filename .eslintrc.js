73affc0f53a875d967919ecfb9f475ffe3b3502c
Andrew Usher <andrewusher00@gmail.com>
2022-02-12 19:30:58 -0600

    chore: update eslint config
    Moved custom parser/rule to shared eslint config, so they're no longer needed here

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['@drewster/eslint-config/react', '@drewster/eslint-config/ts', 'plugin:jest/recommended'],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 0
-  }
+  extends: ['@drewster/eslint-config/react', '@drewster/eslint-config/ts', 'plugin:jest/recommended']
 }
