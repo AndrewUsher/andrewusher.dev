e9528b795b66b87834444bdbdaf8cbe84ac6ecbd
Andrew Usher <andrewusher00@gmail.com>
2022-05-07 13:56:44 -0500

    feat: add cards section to homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react'
+import {
+  BookOpenIcon,
+  HeartIcon,
+  OfficeBuildingIcon,
+  TerminalIcon
+} from '@heroicons/react/solid'
 import { Link, LoaderFunction, useLoaderData } from 'remix'
 import { RecentPosts } from '~/components/home/RecentPosts/RecentPosts'
 import { RecentProjects } from '~/components/home/RecentProjects/RecentProjects'
 import { getBlogPosts, getProjects } from '~/lib/contentful.server'
 
-function Paragraph ({ children }: { children: React.ReactNode }) {
+function Paragraph({ children }: { children: React.ReactNode }) {
   return <p>{children}</p>
 }
 
-function ExternalLink ({ href, children }: { children: string; href: string }) {
+function ExternalLink({ href, children }: { children: string; href: string }) {
   return (
     <a className="underline" href={href}>
       {children}
@@ -16,19 +22,40 @@ function ExternalLink ({ href, children }: { children: string; href: string }) {
   )
 }
 
-function IntroLinkButton ({ children, to }: { children: string; to: string }) {
+type IntroSectionCardProps = {
+  body: string
+  heading: string
+  icon: React.FunctionComponent<{ className: string }>
+  url: string
+}
+
+function IntroSectionCard({
+  body,
+  heading,
+  icon: Icon,
+  url
+}: IntroSectionCardProps) {
   return (
     <Link
-      className="bg-sky-400 dark:bg-sky-600 hover:bg-sky-600 dark:hover:bg-sky-800 hover:text-white mr-8 px-4 py-4 text-lg ease-in-out w-full md:w-auto md:inline-block block text-center mb-8 dark:text-white"
-      to={to}
+      to={url}
+      className="flex items-center rounded shadow-lg card p-6 transition ease-in-out hover:scale-105"
     >
-      {children}
+      <section>
+        <Icon className="h-10 w-10 mr-4 text-sky-600" />
+      </section>
+      <section>
+        <h2 className="font-bold text-3xl mb-1 text-sky-600">{heading}</h2>
+        <p className="text-base">{body}</p>
+      </section>
     </Link>
   )
 }
 
 export const loader: LoaderFunction = async () => {
-  const [blogPosts, projects] = await Promise.all([getBlogPosts(), getProjects()])
+  const [blogPosts, projects] = await Promise.all([
+    getBlogPosts(),
+    getProjects()
+  ])
 
   return {
     recentBlogPosts: blogPosts.items.slice(0, 5).map(({ fields }) => ({
@@ -44,7 +71,7 @@ export const loader: LoaderFunction = async () => {
   }
 }
 
-export default function Index () {
+export default function Index() {
   const { recentBlogPosts, recentProjects } = useLoaderData()
   return (
     <>
@@ -68,12 +95,34 @@ export default function Index () {
             <Link to="/about">Read more about me here.</Link>
           </Paragraph>
         </div>
-        <div className="mt-12">
-          <IntroLinkButton to="/blog">Go To Blog</IntroLinkButton>
-          <IntroLinkButton to="/contact">Get In Touch</IntroLinkButton>
+        <div className="my-12 grid grid-cols-2 gap-8">
+          <IntroSectionCard
+            body="Loose collection of thoughts, things learned, and who-knows-whats about JavaScript, React, and TypeScript"
+            heading="Blog"
+            icon={BookOpenIcon}
+            url="/blog"
+          />
+          <IntroSectionCard
+            body="All of the fun things I've been working on"
+            heading="Projects"
+            icon={OfficeBuildingIcon}
+            url="/projects"
+          />
+          <IntroSectionCard
+            body="List of my daily drivers and necessities anyways, mostly revolving around my love for Apple"
+            heading="Uses"
+            icon={TerminalIcon}
+            url="/uses"
+          />
+          <IntroSectionCard
+            body="A list of things that I like, in no particular order"
+            heading="Things I Like"
+            icon={HeartIcon}
+            url="/things-i-like"
+          />
         </div>
-        <RecentProjects projects={recentProjects}/>
-        <RecentPosts posts={recentBlogPosts}/>
+        <RecentProjects projects={recentProjects} />
+        <RecentPosts posts={recentBlogPosts} />
       </div>
     </>
   )
