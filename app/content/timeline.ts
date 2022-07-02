6186e2021930db01ee35ba1a09669879092c8133
Andrew Usher <andrewusher00@gmail.com>
2022-07-02 12:15:12 -0500

    feat: add timeline to about page

---
+++ app/content/timeline.ts
@@ -0,0 +1,22 @@
+export const timelineData = [
+  {
+    date: 'August 2018',
+    heading: 'Moved into my first apartment 🏠',
+    description: null,
+  },
+  {
+    date: 'May 2018',
+    heading: 'Started First Dev Job @ AutoZone 🚗',
+    description: null,
+  },
+  {
+    date: 'May 2017',
+    heading: 'Graduated highschool',
+    description: null,
+  },
+  {
+    date: 'February 9, 1999',
+    heading: 'I was Born 👶🏾',
+    description: null,
+  },
+]
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react'
 import AboutPageContent from '../content/about.mdx'
+import { timelineData } from '../content/timeline'
 
 export default function AboutPage() {
   return (
     <>
-      <div className="prose mx-auto min-h-[80vh] max-w-screen-xl p-8 dark:prose-invert lg:prose-xl">
-        <AboutPageContent />
+      <div className="mx-auto min-h-[80vh] max-w-screen-xl p-8">
+        <div className="prose mb-8 max-w-screen-xl dark:prose-invert lg:prose-xl">
+          <AboutPageContent />
+        </div>
+        <ol className="relative border-l border-emerald-400 dark:border-emerald-600">
+          {timelineData.map((entry) => (
+            <li className="mb-10 ml-4" key={entry.heading}>
+              <div className="dark:bg-blue-6 00 absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-blue-400  dark:border-gray-900"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                {entry.date}
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                {entry.heading}
+              </h3>
+              {entry.description && (
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  {entry.description}
+                </p>
+              )}
+            </li>
+          ))}
+        </ol>
       </div>
     </>
   )
