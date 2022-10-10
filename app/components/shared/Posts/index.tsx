8a5e9510c881cc3ea86c6b932e0f4acc7c906794
Andrew Usher <andrewusher00@gmail.com>
2022-10-09 19:17:07 -0500

    feat: add animation to blog posts

--- app/components/shared/Posts/index.tsx
+++ app/components/shared/Posts/index.tsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs'
+import { motion } from 'framer-motion'
 import React from 'react'
 import { Link } from 'remix'
 import { BlogPostOrJournalEntry } from '~/types/contentful'
@@ -12,7 +13,13 @@ export function Posts({ entrySlugStart, posts }: Props) {
   return (
     <>
       {posts.map((post) => (
-        <article key={post.title} className="mb-6">
+        <motion.article
+          key={post.title}
+          className="mb-6 origin-left"
+          initial={{ opacity: 0, translateX: -800 }}
+          whileInView={{ opacity: 1, translateX: 0 }}
+          viewport={{ once: true }}
+        >
           <h3 className="mb-1 break-words text-2xl font-semibold tracking-wide dark:text-white">
             <Link
               className="tracking-tighter hover:text-sky-500"
@@ -27,7 +34,7 @@ export function Posts({ entrySlugStart, posts }: Props) {
           >
             Published on {dayjs(post.date).format('MMMM DD, YYYY')}
           </time>
-        </article>
+        </motion.article>
       ))}
     </>
   )
