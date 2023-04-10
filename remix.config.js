dbec62eac4fa53c1342269c0c1f1370c2524add1
Andrew Usher <andrewusher00@gmail.com>
2023-04-09 19:04:35 -0500

    chore: update remix to 1.15.0

--- remix.config.js
+++ remix.config.js
@@ -5,16 +5,24 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'api/_build',
+  serverBuildPath: './api/_build/index.js',
   ignoredRouteFiles: ['.*'],
-  async mdx (filename) {
+  async mdx(filename) {
     const [rehypeTOC, rehypeSlug] = await Promise.all([
       import('@jsdevtools/rehype-toc').then((mod) => mod.default),
-      import('rehype-slug').then((mod) => mod.default)
+      import('rehype-slug').then((mod) => mod.default),
     ])
 
     return {
-      rehypePlugins: [rehypeSlug, [rehypeTOC, { cssClasses: { toc: 'bg-slate-100 dark:bg-slate-800 p-8 shadow-lg' } }]]
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeTOC,
+          {
+            cssClasses: { toc: 'bg-slate-100 dark:bg-slate-800 p-8 shadow-lg' },
+          },
+        ],
+      ],
     }
-  }
+  },
 }
