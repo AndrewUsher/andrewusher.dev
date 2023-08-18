9609c9da68647fbf3f1b679252ccf72e5f17a5b1
Andrew Usher <andrewusher00@gmail.com>
2023-08-18 13:55:24 -0500

    feat: add animation to projects list

--- app/components/home/RecentProjects/RecentProjects.tsx
+++ app/components/home/RecentProjects/RecentProjects.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { Projects } from '~/components/shared/Projects/Projects'
 import { Project } from '~/types/contentful'
 
@@ -8,11 +9,15 @@ type Props = {
 
 export function RecentProjects({ projects }: Props) {
   return (
-    <section>
+    <motion.section
+      initial={{ opacity: 0, y: 400 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, stiffness: 50, delay: 0.2 }}
+    >
       <h2 className="mb-8 text-4xl font-bold tracking-tight dark:text-white">
         Recent Projects
       </h2>
       <Projects projects={projects} />
-    </section>
+    </motion.section>
   )
 }
