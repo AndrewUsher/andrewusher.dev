13681bc406a9a869de4f82e8bdea8d52a80e583e
Andrew Usher <andrewusher00@gmail.com>
2025-06-15 12:48:29 -0500

    chore: add Astro configuration and integrate Tailwind CSS, React, and MDX

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), mdx()],
+})
